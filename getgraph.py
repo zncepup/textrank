f0fb6657cce97b7ab277fe1a57885b20bef0d793
zncepup <1138139812@qq.com>
2017-08-22 16:32:36 +0800

    Add files via upload

---
+++ getgraph.py
@@ -0,0 +1,21 @@
+import wmd
+def filecut(fname,wmd):
+    with open(fname) as f:
+        s=f.read()
+        s=s.split("。")
+    w=[[0 for x in range(len(s))] for x in range(len(s))]
+    for i in range(len(s)):
+        for j in range(len(s)):
+            if j != i:
+                if wmd.compute(s[i],s[j])<999:
+                    w[i][j]=1.0/wmd.compute(s[i],s[j])
+                else:
+                    w[i][j]=0.0
+            else:
+                w[i][j]=0.0
+    return s,w
+
+# wmd=wmd.a()
+# s,w=filecut("test.txt",wmd)
+# print(w)
+
